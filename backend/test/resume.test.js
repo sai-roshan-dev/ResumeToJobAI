67d877a2d8a11d4bec6bb65c42c8d68be5d0a05f
Roshan <neelamsairoshan2003@gmail.com>
2025-08-14 01:44:05 +0530

    Update server.js file

--- backend/test/resume.test.js
+++ backend/test/resume.test.js
@@ -1,5 +1,5 @@
 const request = require('supertest');
-const app = require('../server.cjs');
+const app = require('../server.js');
 const mongoose = require('mongoose');
 const path = require('path');
 
